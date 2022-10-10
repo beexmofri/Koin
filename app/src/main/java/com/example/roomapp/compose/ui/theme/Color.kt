975aca14e6def443b5d4b9a7f321bdfa51522153
Binar <binarmohamadfaruqirayono@gmail.com>
2022-10-10 14:52:37 +0700

    test

---
+++ app/src/main/java/com/example/roomapp/compose/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.example.roomapp.compose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val Red200 = Color(0xFFF51414)
+val Black200 = Color(0xFF000000)
--- app/src/main/java/com/example/roomapp/fragments/login/LoginFragment.kt
+++ app/src/main/java/com/example/roomapp/fragments/login/LoginFragment.kt
@@ -8,9 +8,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement.Center
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.runtime.*
+import androidx.compose.runtime.savedinstancestate.savedInstanceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.findNavController
 import com.example.roomapp.R
+import com.example.roomapp.compose.ui.theme.Black200
+import com.example.roomapp.compose.ui.theme.Red200
 import com.example.roomapp.databinding.FragmentLoginBinding
+import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class LoginFragment : Fragment() {
@@ -24,6 +46,12 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+//        binding.logincompose.setContent {
+////            SimpleTextComponent("Example of Simple Material Text Field")
+//            SimpleMaterialTextFieldComponent()
+//            Divider(color = Color.Gray)
+//            }
         return binding.root
     }
 
@@ -62,5 +90,19 @@ class LoginFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    @ExperimentalFoundationApi
+    @Composable
+    fun SimpleMaterialTextFieldComponent(){
+        var text by savedInstanceState { "" }
+        // TextField is a Material Design implementation of EditText in Compose.
+        // label is the text that is displayed inside the TextField when no text is there
+        TextField(
+            value = text,
+            modifier = Modifier.padding(16.dp).fillMaxWidth(),
+            onValueChange = { text = it },
+            label = { Text("Label") }
+        )
     }
 }
